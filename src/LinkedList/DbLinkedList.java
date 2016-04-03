7cf0fbcad9f7195fdbc1eee540b66e346b7e42ae
dvp236 <dvp236@nyu.edu>
2016-04-02 23:20:00 -0400

    *updated doubly linked list

--- src/LinkedList/DbLinkedList.java
+++ src/LinkedList/DbLinkedList.java
@@ -1,10 +1,74 @@
 package LinkedList;
 
+
 /**
  * 
  * @author dharmik
  *	Implement Doubly Linked List
  */
 public class DbLinkedList {
-
+	Node head,tail;
+	private int lenOfList = 0;
+	
+	private class Node {
+		//pointer to next node
+		Node right,left;
+		int data;
+		
+		public Node(int data){
+			this.data = data;
+			this.right = null;
+			this.left = null;
+		}
+		
+		public void setData(int data){
+			this.data = data;
+		}
+		
+		public int getData(){
+			return this.data;
+		}
+		
+		public void setLeft(Node newNode){
+			this.left = newNode;
+		}
+		
+		public void setRight(Node newNode){
+			this.right = newNode;
+		}
+		
+		public Node getLeft(){
+			return this.left;
+		}
+		
+		public Node getRight(){
+			return this.right;
+		}
+		
+	}
+	
+	public void insert(int n){
+		Node newNode = new Node(n);
+		if(head == null){
+			head = newNode;
+			tail = newNode;
+			lenOfList++;
+			return;
+		}
+		
+		tail.setLeft(newNode);
+		tail = newNode;
+		lenOfList++;
+		
+	}
+	
+	public void printList(){
+		Node curr = head;
+		while(curr != null){
+			System.out.println(curr.getData());
+			curr = curr.getLeft();
+		}
+	}
+	
+	
 }
